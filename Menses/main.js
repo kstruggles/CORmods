c18c550e83f1431b8ec5e9e95ad6a94437579ed2
kstruggles <98306370+kstruggles@users.noreply.github.com>
2022-07-23 00:32:53 -0700

    Update main.js
    changed pregnancy math

--- Menses/main.js
+++ Menses/main.js
@@ -4,15 +4,13 @@
   checkAndAct(characterId) {
     let character = daapi.getCharacter({ characterId })
     let age = daapi.calculateAge({ month: character.birthMonth, year: character.birthYear })
-	if ( (daapi.getState().month === 2 ||daapi.getState().month === 3)  &&
-      !character.isMale &&
+	if (!character.isMale &&
       character.spouseId &&
 	 age >= 14 && 
-	 age <= 40 &&
+	 age <= 40 && 
+	 Math.random() < 1 / 13 / 5 / (character.childrenIds.length || 1) &&
 	 	 !character.startedPregnancyTime 
-		
-		 
-    )
+		  )
 	{
 		daapi.impregnate({ characterId })
     }
@@ -23,7 +21,7 @@ else if
 	 age >= 14 && 
 	 age <= 40 &&
 	 !character.startedPregnancyTime &&
-Math.random() < 0.7/100	 
+Math.random() < 1 / 13 / 5 / (character.childrenIds.length || 1)
     )
 	 
     
